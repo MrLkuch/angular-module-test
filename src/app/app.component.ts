21aef0773c1aa72999088a45d58e2255200d9a84
MrLkuch <kucheida.lucas@gmail.com>
2024-06-18 12:54:59 +0200

    feat(app/): import modules in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeModule } from "./welcome/welcome.module";
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, WelcomeModule, ShoppingListModule]
 })
 export class AppComponent {
   title = 'module-test';
