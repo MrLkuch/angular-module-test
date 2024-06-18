689d4b1da472da231ea49b2265488530bf6a1991
MrLkuch <kucheida.lucas@gmail.com>
2024-06-18 12:54:43 +0200

    feat(welcome/): create greeting component & import it in module

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
-
+import { GreetingMessageComponent } from './greeting-message/greeting-message.component';
 
 
 @NgModule({
   declarations: [
-    WelcomeMessageComponent
+    WelcomeMessageComponent,
+    GreetingMessageComponent,
+
   ],
   imports: [
     CommonModule
   ],
-  exports: [WelcomeMessageComponent],
+  exports: [WelcomeMessageComponent, GreetingMessageComponent],
 })
 export class WelcomeModule { }
